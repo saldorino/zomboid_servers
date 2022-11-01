98c8eac6efe75186ddfa7de2bce2cf15071346e4
Sebastian Aldorino <s.aldorino@gmail.com>
2022-11-01 11:06:28 -0300

    Add winterland v41 files

---
+++ not-gaming-winter-v41_spawnregions.lua
@@ -0,0 +1,12 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "li_township", file = "media/maps/li_township/spawnpoints.lua" },
+		{ name = "Otr", file = "media/maps/Otr/spawnpoints.lua" },
+		{ name = "AZSpawn", file = "media/maps/AZSpawn/spawnpoints.lua" },
+		{ name = "LittleTownship", file = "media/maps/LittleTownship/spawnpoints.lua" },
+	}
+end
